2bbf1a33a1fbe439e372f8c90b6c4c804af91c97
Danila Egorov <ya.ul2010@yandex.ru>
2024-02-19 22:36:13 +0400

    index() is removed from MarkController

--- app/Http/Controllers/MarkController.php
+++ app/Http/Controllers/MarkController.php
@@ -10,14 +10,6 @@ use Illuminate\Http\Request;
 class MarkController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      */
     public function create(Student $student)
